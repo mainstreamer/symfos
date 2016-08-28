47d85b8df3b139ff638e43542f1fbeca618ac62a
Artem Dolmatov <artem.kyiv@outlook.com>
2016-08-28 21:44:21 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+chatbox
+=======
+
+A Symfony project created on August 28, 2016, 7:53 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as Baseuser;
+
+/**
+ * User
+ */
+class User extends Baseuser
+{
+
+    /**
+     * @var string
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     */
+    private $lastName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+}
+
